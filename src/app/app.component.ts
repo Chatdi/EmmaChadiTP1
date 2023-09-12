70128cf19be1c8fdc7bb1c50571a1b58ef62961c
Chatdi <chadi.grolleauraoux@gmail.com>
2023-09-12 16:19:56 +0200

    version 1 (final)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EmmaChadiTP1';
+  colors: string = 'white';
+
+  public changeColor(color:string){
+    this.colors = color;
+  }
 }
+
