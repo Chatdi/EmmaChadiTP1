432c170a3bbdf516304c5b22a9349f6c2b301816
emma <chen.emma1001@gmail.com>
2023-09-12 19:02:36 +0200

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EmmaChadiTP1';
-  colors: string = 'white';
+  colors: string='';
 
   public changeColor(color:string){
     this.colors = color;
